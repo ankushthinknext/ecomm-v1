d3661be985865098418c07fa4b5444b83d9f58b6
accofankush <emailfankush@gmail.com>
2021-09-29 17:49:15 +0530

    Main component done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,35 +2,19 @@ import React, { useEffect, useState } from "react";
 import axios from "../config/axiosConfig";
 import moment from "moment";
 import qs from "query-string";
-import { Container, Grid, Paper, Typography } from "@mui/material";
+import {
+	Container,
+	Grid,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableRow,
+	Typography,
+} from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { Line } from "react-chartjs-2";
-
-const data = {
-	labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-	datasets: [
-		{
-			label: "# of Votes",
-			data: [100, 190, 3, 5, 2, 3, 3],
-			fill: false,
-			backgroundColor: "#f3a7bd",
-			borderColor: "rgba(255, 99, 132, 0.2)",
-			fill: true,
-		},
-	],
-};
-
-const options = {
-	scales: {
-		yAxes: [
-			{
-				ticks: {
-					beginAtZero: true,
-				},
-			},
-		],
-	},
-};
 
 const useStyle = makeStyles({
 	mPanel: {
@@ -48,15 +32,54 @@ const useStyle = makeStyles({
 function Main() {
 	const classes = useStyle();
 	const [dashboardData, setDashboardData] = useState(null);
+	const [recentTransaction, setrecentTransaction] = useState(null);
 	const [query, setQuery] = useState({
 		start: moment().startOf("week").format("llll"),
 		end: moment().endOf("week").format("llll"),
+		limit: 5,
+		sort: "Newest",
 	});
+	const data = {
+		labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+		datasets: [
+			{
+				label: "# of Votes",
+				data: dashboardData && [
+					dashboardData?.items.find((el) => el._id == 1)?.grandtotal,
+					dashboardData?.items.find((el) => el._id == 2)?.grandtotal,
+					dashboardData?.items.find((el) => el._id == 3)?.grandtotal,
+					dashboardData?.items.find((el) => el._id == 4)?.grandtotal,
+					dashboardData?.items.find((el) => el._id == 5)?.grandtotal,
+					dashboardData?.items.find((el) => el._id == 6)?.grandtotal,
+					dashboardData?.items.find((el) => el._id == 7)?.grandtotal,
+				],
+				fill: false,
+				backgroundColor: "#f3a7bd",
+				borderColor: "rgba(255, 99, 132, 0.2)",
+				fill: true,
+			},
+		],
+	};
+	const options = {
+		scales: {
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+					},
+				},
+			],
+		},
+	};
 	console.log(query);
-	console.log(dashboardData);
+	console.log(recentTransaction);
+
 	useEffect(() => {
 		axios(`transaction/dashboard?${qs.stringify(query)}`).then((response) =>
 			setDashboardData(response.data.data),
+		);
+		axios(`transaction?${qs.stringify(query)}`).then((response) =>
+			setrecentTransaction(response.data.data.transactions),
 		);
 	}, []);
 
@@ -106,7 +129,32 @@ function Main() {
 							<Line data={data} options={options} />
 						</Paper>
 					</Grid>
-					<Grid item xs={12} lg={6}></Grid>
+					<Grid item xs={12} lg={6}>
+						<Paper>
+							{recentTransaction && (
+								<Table>
+									<TableHead>
+										<TableRow>
+											<TableCell>Receipt Number</TableCell>
+											<TableCell>Data</TableCell>
+											<TableCell>Quantity</TableCell>
+											<TableCell>Total</TableCell>
+										</TableRow>
+									</TableHead>
+									<TableBody style={{ height: "200px", overflow: "scroll" }}>
+										{recentTransaction.map((transaction) => (
+											<TableRow>
+												<TableCell>{transaction._id}</TableCell>
+												<TableCell>{transaction.createdAt}</TableCell>
+												<TableCell>{transaction.items.length}</TableCell>
+												<TableCell>{transaction.grandtotal}</TableCell>
+											</TableRow>
+										))}
+									</TableBody>
+								</Table>
+							)}
+						</Paper>
+					</Grid>
 				</Grid>
 			</Container>
 		</div>
