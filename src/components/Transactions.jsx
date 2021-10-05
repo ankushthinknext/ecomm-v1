9e4d407699011ef91acfdc0fc3907783d740924d
accofankush <emailfankush@gmail.com>
2021-10-05 18:26:07 +0530

    Transaction section

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,10 +1,115 @@
-import React from "react";
+import { Container, Grid, Paper } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import axios from "../config/axiosConfig";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 function Transactions() {
+	const [allProducts, setAllProducts] = useState(null);
+	const [productsByCategory, setProductsByCategory] = useState(null);
+	const [value, setValue] = React.useState("1");
+
+	useEffect(() => {
+		axios("product/transaction").then((response) => {
+			setAllProducts(response.data.data.all);
+			setProductsByCategory(response.data.data.categories);
+		});
+	}, []);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
 	return (
-		<div>
-			<h4>Tranasactions</h4>
-		</div>
+		<Container>
+			<Grid container>
+				<Grid items xs={12} lg={8}>
+					<Paper>
+						<Box sx={{ width: "100%", typography: "body1" }}>
+							<TabContext value={value}>
+								<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+									<TabList
+										onChange={handleChange}
+										aria-label="lab API tabs example">
+										<Tab label="All" value="0" />
+										{productsByCategory?.map((category, i) => (
+											<Tab label={category.name} value={i + 1} />
+										))}
+									</TabList>
+								</Box>
+								<TabPanel value="0">
+									<Grid container justifyContent="space-evenly">
+										{allProducts?.map((product) => (
+											<Card sx={{ width: "30%" }}>
+												<CardMedia
+													component="img"
+													height="140"
+													image={product.image}
+													alt="green iguana"
+												/>
+												<CardContent>
+													<Typography gutterBottom variant="h5" component="div">
+														{product?.name}
+													</Typography>
+													<Typography variant="body2" color="text.secondary">
+														{product?.description}
+													</Typography>
+												</CardContent>
+												<CardActions>
+													<Button size="small">{product?.price}</Button>
+												</CardActions>
+											</Card>
+										))}
+									</Grid>
+								</TabPanel>
+								{productsByCategory?.map((category, index) => (
+									<TabPanel value={index + 1}>
+										<Grid container justifyContent="space-evenly">
+											{category.items?.map((product) => (
+												<Card sx={{ width: "30%" }}>
+													<CardMedia
+														component="img"
+														height="140"
+														image={product.image}
+														alt="green iguana"
+													/>
+													<CardContent>
+														<Typography
+															gutterBottom
+															variant="h5"
+															component="div">
+															{product?.name}
+														</Typography>
+														<Typography variant="body2" color="text.secondary">
+															{product?.description}
+														</Typography>
+													</CardContent>
+													<CardActions>
+														<Button size="small">{product?.price}</Button>
+													</CardActions>
+												</Card>
+											))}
+										</Grid>
+									</TabPanel>
+								))}
+							</TabContext>
+						</Box>
+					</Paper>
+				</Grid>
+				<Grid items xs={12} lg={4}>
+					<Paper>cart</Paper>
+				</Grid>
+			</Grid>
+		</Container>
 	);
 }
 
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,8 +1,54 @@
-import { Container, Grid, Button } from "@mui/material";
-import React from "react";
+import {
+	Container,
+	Grid,
+	Button,
+	Paper,
+	Table,
+	TableHead,
+	TableCell,
+	TableRow,
+	TableBody,
+	TableContainer,
+} from "@mui/material";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "../config/axiosConfig";
+import moment from "moment";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Swal from "sweetalert2";
 
 function Users() {
+	const [users, setUsers] = useState(null);
+	const [refresh, setRefresh] = useState(false);
+	useEffect(() => {
+		axios("user?limit=100").then((response) =>
+			setUsers(response.data.data.users),
+		);
+	}, [refresh]);
+	const handleDelete = (id) => {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You won't be able to revert this!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#ea5179",
+			cancelButtonColor: "grey",
+			confirmButtonText: "Yes, delete it!",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				axios.delete(`user/${id}`).then((response) => {
+					if (response.status === 200) {
+						Swal.fire("Deleted!", "User deleted!", "success");
+						setRefresh(!refresh);
+					} else {
+						Swal.fire("Opps!", "Something went wrong", "error");
+					}
+				});
+			}
+		});
+	};
+
 	return (
 		<div>
 			<Container>
@@ -15,6 +61,56 @@ function Users() {
 						</Link>
 					</Grid>
 				</Grid>
+				<Paper>
+					<Grid container>
+						<Grid items xs={12}>
+							<Grid container>
+								<Grid items xs={12}>
+									<TableContainer
+										style={{ height: "80vh", overflowY: "scroll" }}>
+										{users ? (
+											<Table>
+												<TableHead>
+													<TableRow>
+														<TableCell>Full Name</TableCell>
+														<TableCell>Username</TableCell>
+														<TableCell>Role</TableCell>
+														<TableCell>Last Active</TableCell>
+														<TableCell></TableCell>
+													</TableRow>
+												</TableHead>
+												<TableBody>
+													{users.map((user) => (
+														<TableRow>
+															<TableCell>{user.fullname}</TableCell>
+															<TableCell>{user.username}</TableCell>
+															<TableCell>{user.role}</TableCell>
+															<TableCell>
+																{moment(user.lastActive).format("llll")}
+															</TableCell>
+															<TableCell>
+																<Button>
+																	<ModeEditIcon />
+																</Button>
+																<Button>
+																	<DeleteIcon
+																		onClick={() => handleDelete(user._id)}
+																	/>
+																</Button>
+															</TableCell>
+														</TableRow>
+													))}
+												</TableBody>
+											</Table>
+										) : (
+											<h2>Loading...</h2>
+										)}
+									</TableContainer>
+								</Grid>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Paper>
 			</Container>
 		</div>
 	);
